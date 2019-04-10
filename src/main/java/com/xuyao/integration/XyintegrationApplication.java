88918996443ffded6574d3dd8162eecd9b79c0c5
xuyao <xuyao0@foxmail.com>
2019-04-11 07:46:45 +0800

    activiti整合 es配置

--- src/main/java/com/xuyao/integration/XyintegrationApplication.java
+++ src/main/java/com/xuyao/integration/XyintegrationApplication.java
@@ -1,14 +1,30 @@
 package com.xuyao.integration;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @MapperScan("com.xuyao.integration.dao")
 public class XyintegrationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(XyintegrationApplication.class, args);
+	}
+
+	/**
+	 * 指定TaskExecutor
+	 * @return
+	 */
+	@Primary
+	@Bean
+	public TaskExecutor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		return executor;
 	}
 }
